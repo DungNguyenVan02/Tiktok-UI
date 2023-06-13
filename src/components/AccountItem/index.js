8ce69d4ee923ec1a77bb553ad462aa4f6149a37c
DungNguyen <nguyenvandung12112002@gmail.com>
2023-06-13 14:48:19 +0700

    xây dựng header 1

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="" alt="" />
+            <div className={cx('infor')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('user')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
