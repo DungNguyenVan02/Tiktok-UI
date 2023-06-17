5a029d11db3c1c7bca83dd26ef10d386c531ffe8
DungNguyenVan02 <nguyenvandung12112002@gmail.com>
2023-06-17 09:22:22 +0700

    Tái cấu trúc dự án

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -15,7 +15,7 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { Link } from 'react-router-dom';
 
-import routesConfig from '~/config/routes';
+import routes from '~/config';
 import Search from '../Search';
 import Button from '~/components/Button';
 import { Menu } from '~/components/Popper';
@@ -23,6 +23,7 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { UploadIcon, MessageIcon, InboxIcon } from '~/components/Icons';
 import Image from '~/Image';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 
@@ -97,7 +98,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo')}>
+                <Link to={config.routes.home} className={cx('logo')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
 
